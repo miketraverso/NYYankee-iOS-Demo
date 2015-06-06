82a871832840fe9c567f3c477df53ce08d5bbb96
Michael Traverso <mike@traversoft.com>
2015-06-05 21:09:57 -0400

    Initial Commit

---
+++ yankee-challenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  yankee-challenge
+//
+//  Created by Michael R Traverso on 6/5/15.
+//  Copyright (c) 2015 hire mike traverso, inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ yankee-challenge/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  yankee-challenge
+//
+//  Created by Michael R Traverso on 6/5/15.
+//  Copyright (c) 2015 hire mike traverso, inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
