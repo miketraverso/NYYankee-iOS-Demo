4def98efee87815c7e4e498853d7d75aefda2582
Michael Traverso <mike@traversoft.com>
2015-06-07 10:26:55 -0400

    Updated setPlayer method for forgotten attributes. ... oops

--- yankee-utils/Player.m
+++ yankee-utils/Player.m
@@ -17,6 +17,8 @@
     if (player) {
         
         player.firstInitial = [NSString replaceNullWithEmptyString:[responseObject objectForKey:@"FirstInitial"]];
+        player.lastInitial = [NSString replaceNullWithEmptyString:[responseObject objectForKey:@"LastInitial"]];
+        player.fullName = [NSString replaceNullWithEmptyString:[responseObject objectForKey:@"FullName"]];
         player.playerID = [[responseObject objectForKey:@"PlayerID"] integerValue];
         player.lastName = [NSString replaceNullWithEmptyString:[responseObject objectForKey:@"LastName"]];
         player.firstName = [NSString replaceNullWithEmptyString:[responseObject objectForKey:@"FirstName"]];
@@ -32,6 +34,7 @@
         player.weightPounds =[[responseObject objectForKey:@"Weight"] integerValue];
         player.position =[[responseObject objectForKey:@"Position"] integerValue];
         player.number =[[responseObject objectForKey:@"Number"] integerValue];
+        player.formalName = [NSString replaceNullWithEmptyString:[responseObject objectForKey:@"FormalName"]];
         
         NSString *url = [NSString replaceNullWithEmptyString:[responseObject objectForKey:@"HeadShotURL"]];
         if ([[[url substringWithRange:NSMakeRange(0, 7)] lowercaseString] isEqualToString:@"http://"]) {
@@ -39,10 +42,6 @@
             player.headShotURL = [NSURL URLWithString:url];
         }
         
-        player.birthCountry = [NSString replaceNullWithEmptyString:[responseObject objectForKey:@"FirstInitial"]];
-        player.birthCountry = [NSString replaceNullWithEmptyString:[responseObject objectForKey:@"LastInitial"]];
-        player.birthCountry = [NSString replaceNullWithEmptyString:[responseObject objectForKey:@"FullName"]];
-        player.birthCountry = [NSString replaceNullWithEmptyString:[responseObject objectForKey:@"FormalName"]];
         player.isPitcher = [[responseObject objectForKey:@"IsPitcher"] boolValue];
     }
 
@@ -88,7 +87,7 @@
 - (NSString*)getWeight {
     
     if (_weightPounds > 0) {
-        return [NSString stringWithFormat:@"%li lbs", _weightPounds];
+        return [NSString stringWithFormat:@"%li lbs", (long)_weightPounds];
     }
     return @"Unknown";
 }
@@ -96,7 +95,7 @@
 - (NSString*)getHeight {
     
     if (_heightInches > 0) {
-        return [NSString stringWithFormat:@"%li'%li\"", _heightInches / 12, _heightInches % 12];
+        return [NSString stringWithFormat:@"%i'%i\"", _heightInches / 12, _heightInches % 12];
     }
     return @"Unknown";
 }
